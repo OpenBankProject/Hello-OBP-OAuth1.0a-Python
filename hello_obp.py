3f539e2f35a2389ecce3a4ad73c7746f8b704f2b
Stefan Bethge <stefan+github@lanpartei.de>
2015-05-08 16:43:22 +0200

    change example target to apisandbox

--- hello_obp.py
+++ hello_obp.py
@@ -5,7 +5,7 @@ from requests_oauthlib import OAuth1Session
 client_key = "youcustomerkey"
 client_secret = "youcustomersecret"
 
-base_url = "https://ulsterbank.openbankproject.com"
+base_url = "https://apisandbox.openbankproject.com"
 request_token_url = base_url + "/oauth/initiate"
 authorization_base_url = base_url + "/oauth/authorize"
 access_token_url = base_url + "/oauth/token"
@@ -24,7 +24,7 @@ openbank.parse_authorization_response(redirect_response)
 openbank.fetch_access_token(access_token_url)
 
 #get accounts for a specific bank
-our_bank = 'ulster-ni'
+our_bank = 'rbs'
 print "Available accounts"
 r = openbank.get(u"{}/obp/v1.2.1/banks/{}/accounts/private".format(base_url, our_bank))
 
@@ -36,13 +36,14 @@ for a in accounts:
 our_account = accounts[0]['id']
 
 print "Get owner transactions"
-r = openbank.get(u"{}/obp/v1.2.1/banks/ulster-ni/accounts/{}/owner/transactions".format(base_url,
+r = openbank.get(u"{}/obp/v1.2.1/banks/{}/accounts/{}/owner/transactions".format(base_url,
+    our_bank,
     our_account), headers= {'obp_limit': '25'})
 transactions = r.json()['transactions']
 print "Got {} transactions".format(len(transactions))
 
 print "Transfer some money"
-send_to = {"bank": "ulster-ni", "account": "current13"}
+send_to = {"bank": "rbs", "account": "savings-kids-john"}
 payload = '{"account_id": "' + send_to['account'] +'", "bank_id": "' + send_to['bank'] + '", "amount": "10" }'
 headers = {'content-type': 'application/json'}
 r = openbank.post(u"{}/obp/v1.2.1/banks/{}/accounts/{}/owner/transactions".format(base_url,
