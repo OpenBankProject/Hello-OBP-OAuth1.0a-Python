59ffacaa229976540ce588483a7e7078d6ec36c1
Stefan Bethge <stefan+github@lanpartei.de>
2015-11-07 19:31:19 +0100

    fix payments 1.4 example

--- hello_payments_v1.4.py
+++ hello_payments_v1.4.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from requests_oauthlib import OAuth1Session
-# oauth flow in simple words: http://pyoauth.readthedocs.org/en/latest/guides/oauth1.html
+import sys
 
-client_key = "youcustomerkey"
-client_secret = "youcustomersecret"
+#Note: in order to use this example, you need to have at least one account
+#that you can send money from (i.e. be the owner).
+
+#enter your api credentials here
+client_key = "yourcustomerkey"
+client_secret = "yourcustomersecretkey"
 
 base_url = "https://apisandbox.openbankproject.com"
 request_token_url = base_url + "/oauth/initiate"
@@ -55,20 +59,33 @@ print challenge_type
 print "Initiate transaction request"
 send_to = {"bank": our_bank, "account": "valid-counterpart-accountid-same-currency"}
 payload = '{"to": {"account_id": "' + send_to['account'] +'", "bank_id": "' + send_to['bank'] + \
-    '"}, "value": {"currency": "GBP", "amount": "1"}, "description": "Description abc", "challenge_type" : "' + \
+    '"}, "value": {"currency": "EUR", "amount": "101"}, "description": "Description abc", "challenge_type" : "' + \
     challenge_type + '"}'
 headers = {'content-type': 'application/json'}
-r = openbank.post(u"{}/obp/v1.4.0/banks/{}/accounts/{}/owner/transaction-request-types/sandbox/transaction-requests".format(base_url,
-    our_bank, our_account), data=payload, headers=headers)
-r_json = r.json()
-challenge_query = r_json['challenge']['id']
-transation_req_id = r_json['transactionRequestId']['value']
+r = openbank.post(u"{}/obp/v1.4.0/banks/{}/accounts/{}/owner/transaction-request-types/{}/transaction-requests".format(
+    base_url, our_bank, our_account, challenge_type), data=payload, headers=headers)
+initiate_response = r.json()
 
-print "Challenge query is {}".format(challenge_query)
- #any number works in sandbox mode
-body = '{"id": "' + challenge_query + '","answer": "123456"}'
-r = openbank.post(u"{}/obp/v1.4.0/banks/{}/accounts/{}/owner/transaction-request-types/sandbox/transaction-requests/{}/challenge".format(base_url,
-    our_bank, our_account, transation_req_id), data=body, headers=headers)
+if "error" in initiate_response:
+    sys.exit("Got an error: " + str(initiate_response))
 
-challenge_response = r.json()
-print "Transaction status: {}".format(challenge_response['status'])
+if (initiate_response['challenge'] != None):
+    #we need to answer the challenge
+    challenge_query = initiate_response['challenge']['id']
+    transation_req_id = initiate_response['id']['value']
+
+    print "Challenge query is {}".format(challenge_query)
+    body = '{"id": "' + challenge_query + '","answer": "123456"}'    #any number works in sandbox mode
+    r = openbank.post(u"{}/obp/v1.4.0/banks/{}/accounts/{}/owner/transaction-request-types/sandbox/transaction-requests/{}/challenge".format(
+        base_url, our_bank, our_account, transation_req_id), data=body, headers=headers
+    )
+
+    challenge_response = r.json()
+    if "error" in challenge_response:
+        sys.exit("Got an error: " + str(challenge_response))
+
+    print "Transaction status: {}".format(challenge_response['status'])
+    print "Transaction created: {}".format(challenge_response["transaction_ids"])
+else:
+    #There was no challenge, transaction was created immediately
+    print "Transaction was successfully created: {}".format(initiate_response["transaction_ids"])
